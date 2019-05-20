0dc13e8051778386237e12eae41d7442ab380b00
Dan <dankrasniak>
2019-05-20 08:01:06 +0200

    Add game mirror, set starting window location

--- src/main/java/rsbot/view/MainWindowView.java
+++ src/main/java/rsbot/view/MainWindowView.java
@@ -4,22 +4,38 @@ import rsbot.MyKeyboardController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class MainWindowView extends JFrame implements Runnable {
 
     private static final int GAME_WIDTH = 740;
     private static final int GAME_HEIGHT = 500;
+    private static final int OFFSET_TO_BE_SPECIFIED = -8;
+    private final Rectangle CAPTURE_RECT = new Rectangle(OFFSET_TO_BE_SPECIFIED, 0, GAME_WIDTH, GAME_HEIGHT);
+    private final Point WINDOW_POSITION = new Point(CAPTURE_RECT.x + CAPTURE_RECT.width + 40, CAPTURE_RECT.y);
+
     private static final int WAIT_TIME = 25;
     private boolean RUNNING = true;
+
+    private BufferedImage gameMirror;
+    private Robot robot;
 
     public void initialize() {
         this.addKeyListener(new MyKeyboardController(this));
 
+        try { this.robot = new Robot(); } catch (AWTException e) {e.printStackTrace();}
+
         this.setSize(new Dimension(GAME_WIDTH, GAME_HEIGHT));
+        this.setLocation(WINDOW_POSITION);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
 
         new Thread(this).start();
+    }
+
+    public void paint(Graphics g) {
+        gameMirror = robot.createScreenCapture(CAPTURE_RECT);
+        g.drawImage(gameMirror, 0, 0, null);
     }
 
     private void applicationLoop() {
@@ -32,7 +48,7 @@ public class MainWindowView extends JFrame implements Runnable {
         repaint();
     }
 
-    //region mandatory
+    //region Runnable implementations - run, close
     public void run() {
         while (RUNNING)
             applicationLoop();
